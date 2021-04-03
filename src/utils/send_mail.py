6d63fbe1e0f585162b7233098ae4028b90454789
felipedmsantos95 <fdms@icomp.ufam.edu.br>
2021-04-03 18:10:57 -0400

    configuring .env

--- src/utils/send_mail.py
+++ src/utils/send_mail.py
@@ -2,18 +2,22 @@ import smtplib
 import email.message
 from datetime import datetime
 from email_body import body
+from decouple import config # use .env to sensitive informations
 
 server = smtplib.SMTP('smtp.gmail.com:587')  
 msg = email.message.Message()
 msg['Subject'] = "Cotação do Arroba do Boi"
   
 # Fazer antes (apenas na 1ª vez): Ativar Aplicativos não Seguros.
-  # Gerenciar Conta Google -> Segurança -> Aplicativos não Seguros -> Habilitar
+# Gerenciar Conta Google -> Segurança -> Aplicativos não Seguros -> Habilitar
 # Caso mesmo assim dê o erro: smtplib.SMTPAuthenticationError: (534,
 # Você faz o login no seu e-mail e depois entra em: https://accounts.google.com/DisplayUnlockCaptcha
-msg['From'] = 'boi.gordo.newsletter@gmail.com'
-msg['To'] = 'felipe.011.ms@gmail.com'
-password = "Boi12345"
+
+#Configure your .env to use the variables bellow
+msg['From'] = config('mail_from')
+msg['To'] = config('mail_to')
+password = config('mail_password')
+
 msg.add_header('Content-Type', 'text/html')
 msg.set_payload(body)
   
@@ -23,10 +27,7 @@ s.starttls()
 # s.login(msg['From'], password)
 # s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
 
-f = open('helloworld.html','w')
 
-f.write(body)
-f.close()
 
 # now = datetime.now()
 # print('Email sent at', now)
